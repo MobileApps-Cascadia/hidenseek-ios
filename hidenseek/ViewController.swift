10d5af4b22603f2a1508fea78c1d0cd83cf16553
juanita <juanitaag1@gmail.com>
2020-04-10 12:42:50 -0700

    Initial Commit

---
+++ hidenseek/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hidenseek
+//
+//  Created by juanita aguilar on 4/10/20.
+//  Copyright © 2020 none. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
