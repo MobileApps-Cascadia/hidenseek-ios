79a10873357104f006c16c5d839cfe39c99b11b1
juanita <juanitaag1@gmail.com>
2020-05-15 23:53:29 -0700

    edit matchesTableView

--- hidenseek/ViewControllers/MatchesTableViewController.swift
+++ hidenseek/ViewControllers/MatchesTableViewController.swift
@@ -13,7 +13,7 @@ class MatchesTableViewController: UITableViewController, UINavigationControllerD
     
     //Arrays for testing
          let titles = ["MatchName1", "MatchName2", "MatchName3","MatchName4"]
-    let dates = ["5/05/20", "4/04/20","1/01/20" ,"11/02/19"]
+   // let dates = ["5/05/20", "4/04/20","1/01/20" ,"11/02/19"]
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -57,7 +57,7 @@ class MatchesTableViewController: UITableViewController, UINavigationControllerD
         let cell = tableView.dequeueReusableCell(withIdentifier: "matchesTableViewCell", for: indexPath) as! MatchesTableViewCell
         // Configure the cell...
         cell.matchNameLabel.text = self.titles[indexPath.row]
-        cell.matchDateCreatedLabel.text = self.dates[indexPath.row]
+       // cell.matchDateCreatedLabel.text = self.dates[indexPath.row]
 
         return cell
     }
