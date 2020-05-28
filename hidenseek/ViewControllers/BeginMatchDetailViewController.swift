54a9363754c76eb7efcd3eb95fc2acb9e9cf23e5
juanita <juanitaag1@gmail.com>
2020-05-27 20:58:00 -0700

    added alert for starting match

--- hidenseek/ViewControllers/BeginMatchDetailViewController.swift
+++ hidenseek/ViewControllers/BeginMatchDetailViewController.swift
@@ -20,6 +20,22 @@ class BeginMatchDetailViewController: UIViewController {
     
     @IBOutlet weak var SearchTimeLabel: UILabel!
     
+    @IBAction func beginMatchButton(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "Starting the Match", message: "The Match Will Begin and the Count Time Timer will start",         preferredStyle: UIAlertController.Style.alert)
+
+                 alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertAction.Style.default, handler: { _ in
+                     //Cancel Action
+                    self.dismiss(animated: true, completion: nil)
+                 }))
+                /* alert.addAction(UIAlertAction(title: "",
+                                               style: UIAlertAction.Style.destructive,
+                                               handler: {(_: UIAlertAction!) in
+                                                 //delete match and go back to MatchesTableview
+                                                  self.dismiss(animated: true, completion: nil)
+                 }))*/
+                 self.present(alert, animated: true, completion: nil)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
