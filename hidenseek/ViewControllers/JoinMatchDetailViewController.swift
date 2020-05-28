6130842527b8f2c356c58d46c300a20f59a5f5ed
juanita <juanitaag1@gmail.com>
2020-05-27 21:03:41 -0700

    added JoinMatchDetailVC

---
+++ hidenseek/ViewControllers/JoinMatchDetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  JoinMatchDetailViewController.swift
+//  hidenseek
+//
+//  Created by juanita aguilar on 5/27/20.
+//  Copyright © 2020 none. All rights reserved.
+//
+
+import UIKit
+
+class JoinMatchDetailViewController: UIViewController {
+
+   
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
